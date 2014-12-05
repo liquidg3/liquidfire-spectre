f96353dbdf2490c69bf71782248e157979a50cd0
Tay <me@taylorrome.ro>
2014-12-04 17:07:43 -0700

    search model

--- models/Search.js
+++ models/Search.js
@@ -1,11 +1,13 @@
 define(['altair/facades/declare',
         'altair/mixins/_DeferredMixin',
         'altair/mixins/_AssertMixin',
+        'altair/facades/mixin',
         'lodash'],
 
     function (declare,
               _DeferredMixin,
               _AssertMixin,
+              mixin,
               _) {
 
         "use strict";
@@ -15,9 +17,15 @@ define(['altair/facades/declare',
             find: function (entityType, options) {
 
                 var _options = options || {},
-                    page     = _options.page,
+                    page     = parseInt(_options.page, 10),
                     sort     = _options.sort,
-                    perPage  = _options.perPage;
+                    event    = _options.event,
+                    searchField = _options.searchField,
+                    searchValue = _options.searchValue,
+                    transform = _options.transform || function (entity) {
+                        return entity.getValues();
+                    },
+                    perPage  = parseInt(_options.perPage, 10);
 
                 this.assertNumeric(page, 'You must pass a "page" option to search.');
                 this.assertNumeric(perPage, 'You must pass a "perPage" option to search.');
@@ -39,16 +47,55 @@ define(['altair/facades/declare',
 
                     }
 
+                    //are we searching?
+                    if (searchField) {
+                        statement.where(searchField, '===', searchValue);
+                    }
+
                     return statement.execute();
 
 
                 }).then(function (cursor) {
 
-                    console.log(cursor);
+                    var results = [],
+                        total    = cursor.total(),
+                        stepping = cursor.each().step(function (entity) {
 
+                        results.push(transform(entity));
 
+                    }).then(function () {
+                        return results;
+                    });
 
-                });
+                    return this.all({
+                        total: total,
+                        results: stepping
+                    });
+
+                }.bind(this));
+
+            },
+
+            findFromEvent: function (entityType, e, options) {
+
+                var request = e.get('request'),
+                    _options = {
+                        perPage:    request.get('perPage'),
+                        page:       request.get('page'),
+                        searchField: request.get('searchField'),
+                        searchValue: request.get('searchValue'),
+                        transform:  function (entity) {
+                            return entity.getHttpResponseValues(e);
+                        }
+                    };
+
+                if (request.get('sortField')) {
+                    _options.sort = {};
+                    _options.sort[request.get('sortField')] = request.get('sortDirection', 'ASC');
+                }
+
+                return this.find(entityType, mixin(_options, options || {}))
+
 
             }
 
